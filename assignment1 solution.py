f1f0dbebe42ce1ddbf768706ccf3afb55182e939
Aditya-9919 <83413964+Aditya-9919@users.noreply.github.com>
2022-04-23 17:50:46 +0530

    Add files via upload

---
+++ assignment1 solution.py
@@ -0,0 +1,32 @@
+Python 3.9.12 (tags/v3.9.12:b28265d, Mar 23 2022, 23:52:46) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: C:/Users/Lenovo/OneDrive/Desktop/assignment1.py ===========
+QUESTION 1 :
+ENTER A NUMBER : 23
+ENTER ANOTHER NUMBER : 23
+ENTER ANOTHER NUMBER : 23
+23.0
+QUESTION 2 :
+ENTER GROSS INCOME : 300000
+ENTER NUMBER OF DEPENDENTS : 2
+56800.0
+QUESTION 3 :
+ENTER YOUR SID : 21102116
+ENTER YOUR NAME : Aditya 
+ENTER YOUR GENDER : male
+ENTER YOUR COURSE NAME : es1101
+ENTER YOUR CGPA9.8
+[21102116, 'Aditya ', 'male', 'es1101', 9.8]
+QUESTION 4 :
+ENTER MARKS OF FIRST STUDENT 72
+ENTER MARKS OF SECOND STUDENT 29
+ENTER MARKS OF THIRD STUDENT23
+ENTER MARKS OF FOURTH STUDENT 23
+ENTER MARKS OF FIFTH STUDENT23
+[23, 23, 23, 29, 72]
+QUESTION 5 :
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+PART 2
+['Red', 'Green', 'White', 'Purple', 'Yellow']
+>>> 
