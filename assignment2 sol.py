1518144bcd6a8d78a9ebb6d0a6f7e2b2a703bad5
Aditya-9919 <83413964+Aditya-9919@users.noreply.github.com>
2022-05-15 16:59:19 +0530

    Add files via upload

---
+++ assignment2 sol.py
@@ -0,0 +1,33 @@
+Python 3.9.12 (tags/v3.9.12:b28265d, Mar 23 2022, 23:52:46) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: C:/Users/Lenovo/OneDrive/Desktop/Assignment2.py ===========
+Enter the string : Aditya Kumar Singh
+Length of the given string:  18
+Reverse string:  hgniS ramuK aytidA
+New string: ar Singh
+New replaced string : Aditya Kumar Singh
+Index of 'a': 5
+String without white spaces : AdityaKumarSingh
+Enter your name :Aditya 
+Enter your sid :21102116
+Enter your department :civil
+Enter your cgpa :7.6
+Hey, Aditya  Here!
+My SID is 21102116
+I am from civil department and my CGPA is 7.6
+a&b : 8
+a|b : 58
+a^b : 50
+Left shift both a and b with 2 bits : a = %d, b = %d (224, 40)
+Right shift a with 2 bits and b with 4 bits : a = %d, b = %d (14, 0)
+Enter a stringAditya
+No
+10
+20
+30
+No
+Enter number a:20
+Enter number b:30
+2
+>>> 
