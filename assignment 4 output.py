944453b2e7638b449b2f70a2d035ede166616e73
Aditya-9919 <83413964+Aditya-9919@users.noreply.github.com>
2022-06-12 16:24:10 +0530

    Add files via upload

---
+++ assignment 4 output.py
@@ -0,0 +1,49 @@
+Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: C:/Users/Lenovo/OneDrive/Desktop/assignment 4.py ==========
+answer1
+Enter Marks :50
+ Grade C 
+Enter a year: 2022
+year is not a Leap Year
+answer3
+Question 1:9X1=9
+Right!
+Question 2:7X5=35
+Right!
+Question 3:7X9=63
+Right!
+Question 4:9X6=9
+Wrong.The right answer is 54
+answer4
+Question 5:3X9=3
+Wrong.The right answer is 27
+answer4
+Question 6:7X8=3
+Wrong.The right answer is 56
+answer4
+Question 7:6X5=3
+Wrong.The right answer is 30
+answer4
+Question 8:6X5=3
+Wrong.The right answer is 30
+answer4
+Question 9:3X9=3
+Wrong.The right answer is 27
+answer4
+Question 10:4X5=3
+Wrong.The right answer is 20
+answer4
+177 candies are in the bowl!
+>>> 3
+3
+>>> 3
+3
+>>> 3
+3
+>>> 3
+3
+>>> 3
+3
+>>> 
