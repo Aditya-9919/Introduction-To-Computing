11ce617eafd5b10163de7e7491b45178e5a2c2ff
Aditya-9919 <83413964+Aditya-9919@users.noreply.github.com>
2022-06-13 09:51:26 +0530

    Update assignment 4.py

--- assignment 4.py
+++ assignment 4.py
@@ -1,4 +1,5 @@
 #Q.1
+#printing result.
 print("answer1")
 m=int(input("Enter Marks :"))
 if(m<25):
@@ -18,6 +19,7 @@ else:
 
 #Q.2
     print("answer2")
+#printing nature of the year.
 year = int(input("Enter a year: "))
 
 if year % 4 == 0 :
@@ -31,6 +33,7 @@ else :
 
 #Q.3
     print("answer3")
+#printing random questions.
 import random
 for i in range(10):
     num1 = random.randint(1,10)
@@ -44,7 +47,7 @@ for i in range(10):
     
 
 #Q.4
-        print("answer4")
+        print("answer4")    
 x=200
 
 for candies in range(x):
