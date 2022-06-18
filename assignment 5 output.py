2effa32a78871c940754cd1b70f3c1226df2c7a9
Aditya-9919 <83413964+Aditya-9919@users.noreply.github.com>
2022-06-18 20:36:41 +0530

    Add files via upload

---
+++ assignment 5 output.py
@@ -0,0 +1,73 @@
+Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: C:/Users/Lenovo/OneDrive/Desktop/Assignment 5.py ==========
+ANS.2
+Enter the minimum value :5
+Enter the maximum value: 60
+Enter the number for divisibility check :4
+8
+12
+16
+20
+24
+28
+32
+36
+40
+44
+48
+52
+56
+60
+ANS.3
+Enter the length of side:3
+Enter the length of side:4
+Enter the length of side:5
+6.0
+ANS.4
+Enter the no. of rows :5
+*
+**
+***
+**
+*
+ANS.5
+Enter the number of rows5
+
+A
+BC
+DEF
+GHIJ
+KLMNO
+ANS.6
+Enter the Lowest Range Value : 3
+Enter the Upper Range Value : 3
+The Prime Numbers in this range are : 
+3
+ANS.7
+The numbers divisible by 11 and are multiple of 7 are : [77, 154, 231, 308, 385, 462]
+ANS.8
+Enter the number 4
+Enter the number 4
+Enter the number 4
+Enter the number 4
+Enter the number 4
+Enter the number 4
+Enter the number 4
+Enter the number 4
+Enter the number 4
+Enter the number 4
+Positive numbers are:  [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+Negative numbers are:  []
+Even numbers are:  [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+Odd numbers are:  []
+Number of times each number occurs in the List {4: 10}
+ANS.9
+Number of words: 4
+Enter the word: sind
+Enter the word: duck
+Enter the word: lol
+Enter the word: lol
+Number of occurences:  {'sind': 1, 'duck': 1, 'lol': 2}
+>>> 
