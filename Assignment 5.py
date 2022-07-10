868e286a9403d17729a4bc00e27f0d402fe83325
Aditya-9919 <83413964+Aditya-9919@users.noreply.github.com>
2022-07-11 01:48:20 +0530

    Update Assignment 5.py

--- Assignment 5.py
+++ Assignment 5.py
@@ -52,20 +52,22 @@ for i in range(1,n):
 
 #question-5---------------------------------------------------------------------------------------------------------------------
 print("ANS.5")
-row=int(input("Enter the number of rows"))
-n=0
-
-for i in range(0,row+1):
-      
-      for j in range(i): 
-            if n==26:
-                n=0 
+row=int(input("enter row"))
+k=0
+for i in range(1,row):
+    
+    for j in range(1,i+1):
+        if j<=i:
+            if k<26:
+                print(chr(65+k),end="")
             else:
-                pass
-            y=chr(65+n) 
-            print(y, end="")
-            n+=1
-      print("")
+                k=0
+                print(chr(65+k),end="")
+           
+            k+=1
+    print()
+
+
 
 
 
